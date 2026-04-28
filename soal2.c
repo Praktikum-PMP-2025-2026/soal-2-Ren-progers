ed94a27d9a6d3d8a1db3cf0bed75061bb35cecbc
Ren-progers <kidunggamel@gmail.com>
2026-04-28 09:20:55 +0700

    Update soal2.c
    change1

--- soal2.c
+++ soal2.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
-
-int main(void) {
+#include <stdlib.h>
+#include <string.h>
+typedef struct data
+{
+    char name[50];
+}invent;
+int main (){
+    int N, count = 0, k = 0;
+    char buffer[50], pref [10];
+    scanf ("%d", &N);
+    invent *arr = (invent*) malloc (N * sizeof(invent));
+    for (int i = 0; i < N; i++){
+        scanf ("%s", arr[i].name);
+    }
+    scanf ("%s", pref);
+    for (int i = 0; i < N; i++){
+        for (int j = i+1; j < N; j++){
+            if (strcmp (arr[i].name, arr[j].name) == 1){
+                strcpy (buffer, arr[i].name);
+                strcpy (arr[i].name, arr[j].name);
+                strcpy (arr[j].name, buffer);
+            }
+        }
+    }
+    while (k < N && count != 3){
+        if (strncmp (arr[k].name, pref, strlen (pref)) == 0){
+            count++;
+        }
+        if (count == 1){
+            printf ("SUGGESTION ");
+            printf ("%s ", arr[k].name);
+        }
+        else if (count == 3) {
+            printf ("%s\n", arr[k].name);
+        }
+        else if (count != 0){
+            printf ("%s ", arr[k].name);
+        }
+        k++;
+    }
+    if (count == 0){
+        printf ("TIDAK ADA\n");
+    }
+    
     return 0;
 }
